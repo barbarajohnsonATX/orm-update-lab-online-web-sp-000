72ce00ff9f1da07a4173c2da0e046d4cc1b5a969
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-09 01:54:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,8 @@ require_relative "../config/environment.rb"
 require 'pry'
 
 class Student
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id
   
   
   # Remember, you can access your database connection anywhere in this class
