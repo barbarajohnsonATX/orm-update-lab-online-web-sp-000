fbd85c054ee551c63bec8e0302cc685b62137b38
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-09 01:22:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,17 +31,19 @@ class Student
     DB[:conn].execute(sql)    
   end 
   
-  def save 
-      def save 
-    sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
-    SQL
- 
-    DB[:conn].execute(sql, self.name, self.grade)
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO songs (name, album)
+        VALUES (?, ?)
+      SQL
     
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  end 
+      DB[:conn].execute(sql, self.name, self.album)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
+end
   
   
   
