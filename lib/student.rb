63432492b4779ebf1d1902aa871abb11d4328f32
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-09 01:57:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,10 +34,7 @@ class Student
     DB[:conn].execute(sql)    
   end 
   
-  def update 
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end 
+
   
   def save
     if self.id
@@ -53,7 +50,11 @@ class Student
     end
   end
   
-
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end 
+  
   def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
