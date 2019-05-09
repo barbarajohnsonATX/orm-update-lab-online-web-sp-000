9b7cab12c4902d567b23b9b86c1046ab8c570740
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-09 02:04:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,9 @@
 require_relative "../config/environment.rb"
 
-require 'pry'
-
 class Student
   attr_accessor :name, :grade
   attr_reader :id
   
-  
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
   def initialize(name, grade, id = nil )
     @name = name 
     @grade = grade 
@@ -24,19 +19,21 @@ class Student
         grade FLOAT
         )
         SQL
+        
     DB[:conn].execute(sql)
   end 
   
-    def self.drop_table 
+  def self.drop_table 
      sql =  <<-SQL 
       DROP TABLE students
-        SQL
+      SQL
+        
     DB[:conn].execute(sql)    
   end 
   
 
-  
   def save
+    #if id already exists, just update, don't save
     if self.id
       self.update
     else
@@ -66,14 +63,14 @@ class Student
     student
   end
   
-    def self.new_from_db(row)
+  def self.new_from_db(row)
     # create a new Student object given a row from the database
     new_student = self.new(row[1], row[2], row[0])
     new_student
   end
   
   
-    def self.find_by_name(name)
+  def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
